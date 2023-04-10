4d2580291cd987ad12dcff58c6c17fc86c0eb80a
abukauthar vunde <97521843+ABUKAUTHAR2@users.noreply.github.com>
2023-04-10 10:45:35 +0300

    1234567

--- AddNews.js
+++ AddNews.js
@@ -1,244 +1,111 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, TextInput, StyleSheet, KeyboardAvoidingView, Image } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput, Button, Alert, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
-class AddNews extends Component {
-  state = {
-    context: '',
-    fileData: null,
-    summary: '',
-    description: '',
-    date: new Date(),
-    contextError: '',
-    fileDataError: '',
-    summaryError: '',
-    descriptionError: '',
-    dateError: '',
-    image: null,
+export default class SubmitForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      image: null,
+    };
+  }
+
+  // Handle changes to the name input field
+  handleNameChange = (text) => {
+    this.setState({ name: text });
   };
 
-  handleContextChange = (context) => {
-    if (context.length <= 30) {
-      this.setState({ context, contextError: '' });
-    } else {
-      this.setState({ contextError: 'Context should be less than 30 characters' });
-    }
-  };
-
-  handleFileDataChange = async () => {
+  // Handle selection of an image from the device
+  handleImageSelect = async () => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (status !== 'granted') {
-      alert('Sorry, we need camera roll permissions to make this work!');
+      Alert.alert('Permission needed', 'Please allow access to the photo library.');
       return;
     }
-  
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
+      aspect: [4, 3],
       quality: 1,
     });
-  
+
     if (!result.canceled) {
-      const selectedAsset = result.assets[0];
-      this.setState({ 
-        fileData: selectedAsset,
-        fileDataError: '',
-        image: selectedAsset.uri 
-      });
-    } else {
-      this.setState({ fileData: null, fileDataError: 'Please select a file' });
-    }
-  };
-  
-
-  handleSummaryChange = (summary) => {
-    if (summary.length <= 50) {
-      this.setState({ summary, summaryError: '' });
-    } else {
-      this.setState({ summaryError: 'Summary should be less than 50 words' });
+      this.setState({ image: result.assets[0].uri });
     }
   };
 
-  handleDescriptionChange = (description) => {
-    if (description.length <= 250) {
-      this.setState({ description, descriptionError: '' });
-    } else {
-      this.setState({ descriptionError: 'Description should be less than 250 words' });
-    }
-  };
-
+  // Handle submission of the form
   handleSubmit = () => {
-    const { image, context, summary, description, date } = this.state;
-  
-    let contextError = '';
-    let fileDataError = '';
-    let summaryError = '';
-    let descriptionError = '';
-    let dateError = '';
-  
-    if (context === '') {
-      contextError = 'Please enter context';
-    }
-  
-    if (!image) {
-      fileDataError = 'Please select a file';
-    }
-  
-    if (summary === '') {
-      summaryError = 'Please enter summary';
-    }
-  
-    if (description === '') {
-      descriptionError = 'Please enter description';
-    }
-  
-    if (contextError !== '' || fileDataError !== '' || summaryError !== '' || descriptionError !== '') {
-      this.setState({ contextError, fileDataError, summaryError, descriptionError, dateError });
-      return;
-    }
-  
-    fetch('http://192.168.125.85:80/apis/addnews.php', {
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    image: image,
-    context: context,
-    summary: summary,
-    description: description,
-    date: date,
-  }),
-})
-  .then((response) => response.text())
-  .then((responseJson) => {
-    console.log(responseJson);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-  
+    const formData = new FormData();
+    formData.append('name', this.state.name);
+    formData.append('image', {
+      uri: this.state.image,
+      type: 'image/jpeg',
+      name: 'image.jpg',
+    });
 
+    fetch('http://192.168.125.85:80/apis/submit.php', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        // Show an alert indicating success or failure
+        Alert.alert(
+          responseJson.success ? 'Success' : 'Error',
+          responseJson.message
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
-        
-      render() {
-      const { context, fileData, summary, description, contextError, fileDataError, summaryError, descriptionError } = this.state;
-      
-      return (
-        <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-          <View style={styles.form}>
-            <Text style={styles.heading}>ADD NEWS TO THE APP DATABASE </Text>
-            <Text style={styles.label}>Context:</Text>
-            <TextInput
-              value={context}
-              onChangeText={this.handleContextChange}
-              placeholder="Enter context"
-              style={styles.input}
-            />
-            {contextError !== '' && <Text style={styles.error}>{contextError}</Text>}
-      
-            <Text style={styles.label}>File:</Text>
-            <TouchableOpacity onPress={this.handleFileDataChange} style={styles.input}>
-              <Text>Select a file</Text>
-            </TouchableOpacity>
-            {fileDataError !== '' && <Text style={styles.error}>{fileDataError}</Text>}
-      
-            {fileData && (
-              <View style={styles.imagePreview}>
-                <Image source={{ uri: fileData.uri }} style={styles.image} />
-              </View>
-            )}
-      
-            <Text style={styles.label}>Summary:</Text>
-            <TextInput
-              value={summary}
-              onChangeText={this.handleSummaryChange}
-              placeholder="Enter summary"
-              style={styles.input}
-            />
-            {summaryError !== '' && <Text style={styles.error}>{summaryError}</Text>}
-      
-            <Text style={styles.label}>Description:</Text>
-            <TextInput
-              value={description}
-              onChangeText={this.handleDescriptionChange}
-              placeholder="Enter description"
-              style={styles.input}
-              multiline
-              numberOfLines={5}
-            />
-            {descriptionError !== '' && <Text style={styles.error}>{descriptionError}</Text>}
-      
-            <TouchableOpacity onPress={this.handleSubmit} style={styles.button}>
-              <Text style={styles.buttonText}>Submit</Text>
-            </TouchableOpacity>
-          </View>
-        </KeyboardAvoidingView>
-      );
-    }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.label}>Name:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={this.handleNameChange}
+          value={this.state.name}
+        />
+
+        <Text style={styles.label}>Image:</Text>
+        <Button title="Select Image" onPress={this.handleImageSelect} />
+        {this.state.image && <Image source={{ uri: this.state.image }} style={styles.image} />}
+
+        <Button title="Submit" onPress={this.handleSubmit} />
+      </View>
+    );
   }
-  
-  const styles = StyleSheet.create({
+}
+
+const styles = StyleSheet.create({
   container: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  },
-  heading:{
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#4CAF50',
-    letterSpacing: 1,
-    textTransform: 'uppercase',
-    textAlign: 'center',
-    marginVertical: 20,
-  }
-  ,
-  form: {
-  width: '80%',
+    flex: 1,
+    margin: 20,
+    justifyContent: 'center',
   },
   label: {
-  marginVertical: 5,
+    fontSize: 20,
+    marginBottom: 10,
   },
   input: {
-  borderWidth: 1,
-  borderColor: 'black',
-  borderRadius: 5,
-  padding: 10,
-  marginBottom: 10,
-  },
-  error: {
-  color: 'red',
-  marginBottom: 10,
-  },
-  button: {
-  backgroundColor: '#4CAF50',
-  padding: 10,
-  borderRadius: 5,
-  alignItems: 'center',
-  marginTop: 10,
-  },
-  buttonText: {
-  color: 'white',
-  },
-  imagePreview: {
-  borderWidth: 1,
-  borderColor: 'black',
-  borderRadius: 5,
-  padding: 10,
-  marginBottom: 10,
-  alignItems: 'center',
+    fontSize: 16,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    marginBottom: 20,
   },
   image: {
-  width: 200,
-  height: 200,
+    width: 200,
+    height: 200,
+    marginTop: 10,
+    marginBottom: 20,
   },
-  });
-  
-  export default AddNews;
-  
-  
-  
-              
+});
